6890c7e382dc7c5978975e168d43fe05a5c88113
ArianeValasques <ariane.souza288@gmail.com>
2023-07-21 20:44:12 -0300

    Create knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+const { db } = require('./.env')
+//Por segurança, os dados de conexão com o banco de dados são inseridos no arquivo .env na variável db e requeridos aqui para a conexão.
+
+module.exports = {
+  client: 'postgresql',
+  connection: db,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    //Diretório onde serão criadas e manipuladas das migrations
+    directory: './src/database/migrations'
+  },
+  seeds: {
+    //Diretório onde serão criadas e manipuladas as seeds
+    directory: './src/database/seeds'
+  }
+};
